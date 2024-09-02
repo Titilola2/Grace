0621cf8153c0ad7e38d0d1985faab46bdf5051a4
Grace Olafare <oluwafunhmi@gmail.com>
2024-09-02 07:01:30 -0400

    tech stack update

--- domain/homepage/components/MoreBooks.tsx
+++ domain/homepage/components/MoreBooks.tsx
@@ -31,17 +31,17 @@ const MoreBooks = () => {
     <section className="w-full bg-white py-4 lg:py-[3rem] flex flex-col items-center justify-center gap-[2rem] lg:px-[8rem]  px-[1rem] sidebar">
       <h1 className="font-custom text-center text-red-800 text-4xl font-bold ">Technology stack</h1>
       <div className="h-[30rem]  w-full  rounded-xl shadow-xl grid grid-cols-3 p-8 text-red-900 place-items-center justify-items-center items-center">
-        <div className=""> <TiHtml5 className={` ${isGlowing ? 'h-[5rem] w-[4rem]' : 'h-[3.5rem] w-[3.5rem]'}`} /></div>
-        <div className=""> <IoLogoCss3 className={` ${isGlowing ? 'h-[5rem] w-[4rem]' : 'h-[3.5rem] w-[3.5rem]'}`} /></div>
-        <div className=""> <TbBrandJavascript className={` ${isGlowing ? 'h-[5rem] w-[4rem]' : 'h-[3.5rem] w-[3.5rem]'}`} /> </div>
-        <div className=""> <IoLogoReact className={` ${isGlowing ? 'h-[5rem] w-[4rem]' : 'h-[3.5rem] w-[3.5rem]'}`} /></div>
-        <div className="">  <TbBrandNextjs className={` ${isGlowing ? 'h-[5rem] w-[4rem]' : 'h-[3.5rem] w-[3.5rem]'}`} /></div>
-        <div className=""> <BiLogoTailwindCss className={` ${isGlowing ? 'h-[5rem] w-[4rem]' : 'h-[3.5rem] w-[3.5rem]'}`} /> </div>
+        <div className=""> <TiHtml5 className={`text-orange-700 ${isGlowing ? 'h-[5rem] w-[4rem]' : 'h-[3.5rem] w-[3.5rem]'}`} /></div>
+        <div className=""> <IoLogoCss3 className={` text-amber-400 ${isGlowing ? 'h-[5rem] w-[4rem]' : 'h-[3.5rem] w-[3.5rem]'}`} /></div>
+        <div className=""> <TbBrandJavascript className={` text-orange-700 ${isGlowing ? 'h-[5rem] w-[4rem]' : 'h-[3.5rem] w-[3.5rem]'}`} /> </div>
+        <div className=""> <IoLogoReact className={` text-sky-800 ${isGlowing ? 'h-[5rem] w-[4rem]' : 'h-[3.5rem] w-[3.5rem]'}`} /></div>
+        <div className="">  <TbBrandNextjs className={`text-black ${isGlowing ? 'h-[5rem] w-[4rem]' : 'h-[3.5rem] w-[3.5rem]'}`} /></div>
+        <div className=""> <BiLogoTailwindCss className={` text-teal-500 ${isGlowing ? 'h-[5rem] w-[4rem]' : 'h-[3.5rem] w-[3.5rem]'}`} /> </div>
         <div className=""> <TbBrandTypescript className={` ${isGlowing ? 'h-[5rem] w-[4rem]' : 'h-[3.5rem] w-[3.5rem]'}`} /> </div>
-        <div className=""> <FaPython className={` ${isGlowing ? 'h-[5rem] w-[4rem]' : 'h-[3.5rem] w-[3.5rem]'}`} /> </div>
+        <div className=""> <FaPython className={`text-indigo-900 ${isGlowing ? 'h-[5rem] w-[4rem]' : 'h-[3.5rem] w-[3.5rem]'}`} /> </div>
         <div className=""> <DiRuby className={` ${isGlowing ? 'h-[5rem] w-[4rem]' : 'h-[3.5rem] w-[3.5rem]'}`} /> </div>
-        <div className=""><FaNode className={` ${isGlowing ? 'h-[5rem] w-[4rem]' : 'h-[3.5rem] w-[3.5rem]'}`} />  </div>
-        <div className=""><RiBootstrapLine className={` ${isGlowing ? 'h-[5rem] w-[4rem]' : 'h-[3.5rem] w-[3.5rem]'}`} /> </div>
+        <div className=""><FaNode className={`text-lime-700  ${isGlowing ? 'h-[5rem] w-[4rem]' : 'h-[3.5rem] w-[3.5rem]'}`} />  </div>
+        <div className=""><RiBootstrapLine className={`text-violet-800 ${isGlowing ? 'h-[5rem] w-[4rem]' : 'h-[3.5rem] w-[3.5rem]'}`} /> </div>
         <div className="">  <SiRubyonrails className={` ${isGlowing ? 'h-[5rem] w-[4rem]' : 'h-[3.5rem] w-[3.5rem]'}`} /></div>
       </div>
     </section>
